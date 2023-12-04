99be5be401ae072863d9e68fcb9f544cf5f11506
srichman333 <sarah_n_richman@brown.edu>
2023-12-03 22:48:29 -0500

    barn + grass start

---
+++ barn.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three'
+
+class Barn extends THREE.Mesh{
+  shapesList = []
+  
+  constructor() {
+    super()
+
+    this.makeBarn();
+    this.makeFrontWindows();
+    this.makeSideWindows(true);
+    this.makeSideWindows(false);
+
+    this.shapesList.forEach(each => each.translate(12, 3, 8));
+  }
+
+  translate(x, z) {
+    this.shapesList.forEach(each => each.translate(x, 0, z));
+  }
+
+  makeBarn() {
+    const barnTexture = new THREE.TextureLoader().load('textures/redWoodTexture.png');
+    const barnMaterial = new THREE.MeshPhongMaterial({ map: barnTexture });
+    const roofTexture = new THREE.TextureLoader().load('textures/roofTexture.webp');
+    roofTexture.wrapS = THREE.RepeatWrapping;
+    roofTexture.wrapT = THREE.RepeatWrapping;
+    roofTexture.repeat.set(4,4);
+
+    const roofMaterial = new THREE.MeshPhongMaterial({ map: roofTexture });
+
+    const barnBox = new THREE.BoxGeometry(8, 5, 10);
+    this.add(new THREE.Mesh(barnBox, barnMaterial));
+    this.shapesList.push(barnBox)
+
+    const roof = new THREE.CylinderGeometry( 5, 5, 10, 3, 1, false, 0, 2*Math.PI)
+    roof.rotateX(Math.PI/2).rotateZ(Math.PI/3).translate(0, 5, 0);
+    this.add(new THREE.Mesh(roof, roofMaterial))
+    this.shapesList.push(roof)
+  }
+
+  makeFrontWindows() {
+    const material = new THREE.MeshPhongMaterial({color: "white"})
+
+    const l1 = new THREE.BoxGeometry(.1, 4, .1);
+    l1.translate(-2,-.5,5);
+    this.add(new THREE.Mesh(l1, material))
+    this.shapesList.push(l1)
+    const l2 = new THREE.BoxGeometry(.1, 4, .1);
+    l2.translate(2,-.5,5);
+    this.add(new THREE.Mesh(l2, material))
+    this.shapesList.push(l2)
+
+    const l3 = new THREE.BoxGeometry(4.1, .1, .1);
+    l3.translate(0,1.5,5);
+    this.add(new THREE.Mesh(l3, material))
+    this.shapesList.push(l3)
+
+    const l4 = new THREE.BoxGeometry(Math.sqrt(32), .1, .1);
+    l4.translate(-1,-1,5).rotateZ(Math.PI/4);
+    this.add(new THREE.Mesh(l4, material))
+    this.shapesList.push(l4)
+    const l5 = new THREE.BoxGeometry(Math.sqrt(32), .1, .1);
+    l5.translate(1,-1,5).rotateZ(-Math.PI/4);
+    this.add(new THREE.Mesh(l5, material))
+    this.shapesList.push(l5)
+
+    const l6 = new THREE.BoxGeometry(4.1, .1, .1);
+    l6.translate(0,.5,5);
+    this.add(new THREE.Mesh(l6, material))
+    this.shapesList.push(l6)
+
+    const l7 = new THREE.BoxGeometry(.1, 1, .1);
+    l7.translate(0,1,5);
+    this.add(new THREE.Mesh(l7, material))
+    this.shapesList.push(l7)
+    const l8 = new THREE.BoxGeometry(.1, 1, .1);
+    l8.translate(-1,1,5);
+    this.add(new THREE.Mesh(l8, material))
+    this.shapesList.push(l8)
+    const l9 = new THREE.BoxGeometry(.1, 1, .1);
+    l9.translate(1,1,5);
+    this.add(new THREE.Mesh(l9, material))
+    this.shapesList.push(l9)
+  }  
+
+  makeSideWindows(left) {
+    const x = left? -4 : 4;
+    const material = new THREE.MeshPhongMaterial({color: "white"})
+
+    for (let i=-1; i<2; i++){
+        const sq11 = new THREE.BoxGeometry(.1, 1.5, .1);
+        sq11.translate(x,0,-.75 + (2*i));
+        this.add(new THREE.Mesh(sq11, material))
+        this.shapesList.push(sq11)
+        const sq12 = new THREE.BoxGeometry(.1, 1.5, .1);
+        sq12.translate(x,0,.75 + (2*i));
+        this.add(new THREE.Mesh(sq12, material))
+        this.shapesList.push(sq12)
+        const sq13 = new THREE.BoxGeometry(.1, .1, 1.5);
+        sq13.translate(x,-.75,0 + (2*i));
+        this.add(new THREE.Mesh(sq13, material))
+        this.shapesList.push(sq13)
+        const sq14 = new THREE.BoxGeometry(.1, .1, 1.5);
+        sq14.translate(x,.75,0 + (2*i));
+        this.add(new THREE.Mesh(sq14, material))
+        this.shapesList.push(sq14)
+        const sq15 = new THREE.BoxGeometry(.1, .1, Math.sqrt(2*Math.pow(1.5,2)));
+        sq15.translate(x,(i*1.5),0 + (1.4*i)).rotateX(Math.PI/4);
+        this.add(new THREE.Mesh(sq15, material))
+        this.shapesList.push(sq15)
+    }
+  }
+}
+
+export { Barn }
--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Grass } from "./grass/grass";
+import { Barn } from "./barn";
 
+// scene
 const canvas = document.querySelector("canvas.webgl");
-
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(10, 1, 10);
-geometry.translate(0, -1, 0);
-const material = new THREE.MeshPhongMaterial({ color: "green" });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const sizes = {
+  width: 1400,
+  height: 800,
+};
 
+// lighting
 const light = new THREE.DirectionalLight("white", 10)
 light.position.set(1,2,1)
 scene.add(light)
-
 const ambient_lighting = new THREE.AmbientLight(0x404040, 10)
 scene.add(ambient_lighting)
 
-const sizes = {
-  width: 800,
-  height: 600,
-};
+// adding objects
+const grass = new Grass(50, 500000)
+scene.add(grass)
+const barn = new Barn();
+scene.add(barn)
+
+// camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 10;
+camera.position.z = 20;
+camera.position.y = 1.7;
 camera.lookAtVector = new THREE.Vector3(0,0,-1);
 camera.getWorldDirection(camera.lookAtVector);
 scene.add(camera);
 
+// renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
 
+// movement
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.keys = {
 	LEFT: 'KeyA', //left arrow
@@ -42,15 +48,13 @@ controls.keys = {
   FORWARD: 'KeyW'
 }
 controls.listenToKeyEvents(document);
-
 document.addEventListener("keydown", onKeyDown, false);
 function onKeyDown(event) {
   var keyCode = event.which;
-  var offset = .1;
+  var offset = .2;
   var up = new THREE.Vector3();
   up.copy(camera.up).applyQuaternion(camera.quaternion);
   camera.getWorldDirection(camera.lookAtVector);
-  var left = up.cross(camera.lookAtVector).normalize();
   if (keyCode == 87) { // W key
     camera.position.x += camera.lookAtVector.x*offset;
     camera.position.y += camera.lookAtVector.y*offset;
@@ -59,25 +63,13 @@ function onKeyDown(event) {
     camera.position.x -= camera.lookAtVector.x*offset;
     camera.position.y -= camera.lookAtVector.y*offset;
     camera.position.z -= camera.lookAtVector.z*offset;
-  // } if (keyCode == 65) { // A key
-  //   camera.position.x += left.x*offset;
-  //   camera.position.y += left.y*offset;
-  //   camera.position.z += left.z*offset;
-  // } if (keyCode == 68) { // D key
-  //   camera.position.x -= left.x*offset;
-  //   camera.position.y -= left.y*offset;
-  //   camera.position.z -= left.z*offset;
-  // } if (keyCode == 32) { // space
-  //   camera.position.y += offset;
   } if (keyCode == 91) { // command
     camera.position.y -= offset;
   }
-  renderer.render(scene, camera);
 }
 
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
+// animate
+renderer.setAnimationLoop((time) => {
+  grass.update(time)
+  renderer.render(scene, camera)
+})
