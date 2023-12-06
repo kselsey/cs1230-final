a5e646331869a11634d48ee8cc69f5cdf6c643fa
lmursalova <lmursalova803@gmail.com>
2023-12-05 22:37:01 -0500

    feat: added skybox, switched to PointerLockControls

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 import { Grass } from "./grass/grass";
 import { Barn } from "./barn";
+import { Skybox } from "./skybox.js"
 
 // scene
 const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 const sizes = {
-  width: 1400,
-  height: 800,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 // lighting
@@ -19,15 +22,16 @@ const ambient_lighting = new THREE.AmbientLight(0x404040, 10)
 scene.add(ambient_lighting)
 
 // adding objects
+const skybox = new Skybox();
+scene.add(skybox);
 const grass = new Grass(50, 500000)
 scene.add(grass)
 const barn = new Barn();
 scene.add(barn)
 
 // camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 20;
-camera.position.y = 1.7;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 30000);
+camera.position.set(0, 2, 20);
 camera.lookAtVector = new THREE.Vector3(0,0,-1);
 camera.getWorldDirection(camera.lookAtVector);
 scene.add(camera);
@@ -38,38 +42,67 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-// movement
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.keys = {
-	LEFT: 'KeyA', //left arrow
-	UP: 'Space', // up arrow
-	RIGHT: 'KeyD', // right arrow
-	BOTTOM: 'ControlLeft', // down arrow
-  FORWARD: 'KeyW'
-}
-controls.listenToKeyEvents(document);
+// Camera controls
+const controls = new PointerLockControls(camera, renderer.domElement);
+controls.addEventListener("change", () => console.log("changed"));
+controls.addEventListener("lock", () => console.log("locked"));
+controls.addEventListener("unlock", () => console.log("unlocked"));
+// Lock pointer on mouse click (unlock with escape)
+document.addEventListener("mousedown", () => controls.lock());
 document.addEventListener("keydown", onKeyDown, false);
 function onKeyDown(event) {
   var keyCode = event.which;
-  var offset = .2;
-  var up = new THREE.Vector3();
-  up.copy(camera.up).applyQuaternion(camera.quaternion);
-  camera.getWorldDirection(camera.lookAtVector);
-  if (keyCode == 87) { // W key
-    camera.position.x += camera.lookAtVector.x*offset;
-    camera.position.y += camera.lookAtVector.y*offset;
-    camera.position.z += camera.lookAtVector.z*offset;
-  } if (keyCode == 83) { // S key
-    camera.position.x -= camera.lookAtVector.x*offset;
-    camera.position.y -= camera.lookAtVector.y*offset;
-    camera.position.z -= camera.lookAtVector.z*offset;
-  } if (keyCode == 91) { // command
-    camera.position.y -= offset;
+  var offset = 0.08;
+  // If W, move forward
+  if (keyCode == 87) {
+    controls.moveForward(2 * offset);
+  }
+  // If S, move backward
+  if (keyCode == 83) {
+    controls.moveForward(-2 * offset);
+  }
+  // If D, move right
+  if (keyCode == 68) {
+    controls.moveRight(2 * offset);
+  }
+  // If A, move left
+  if (keyCode == 65) {
+    controls.moveRight(-2 * offset);
   }
 }
 
+// const controls = new FirstPersonControls(camera, renderer.domElement);
+// controls.movementSpeed = 0.2;
+
+// const controls = new OrbitControls( camera, renderer.domElement );
+// controls.keys = {
+// 	LEFT: 'KeyA',
+// 	UP: 'KeyQ',
+// 	RIGHT: 'KeyD',
+// 	BOTTOM: 'KeyE'
+// }
+// controls.listenToKeyEvents(document);
+// document.addEventListener("keydown", onKeyDown, false);
+// function onKeyDown(event) {
+//   var keyCode = event.which;
+//   var offset = .2;
+//   var up = new THREE.Vector3();
+//   up.copy(camera.up).applyQuaternion(camera.quaternion);
+//   camera.getWorldDirection(camera.lookAtVector);
+//   if (keyCode == 87) { // W key
+//     camera.position.x += camera.lookAtVector.x*offset;
+//     camera.position.y += camera.lookAtVector.y*offset;
+//     camera.position.z += camera.lookAtVector.z*offset;
+//   } if (keyCode == 83) { // S key
+//     camera.position.x -= camera.lookAtVector.x*offset;
+//     camera.position.y -= camera.lookAtVector.y*offset;
+//     camera.position.z -= camera.lookAtVector.z*offset;
+//   }
+// }
+
 // animate
 renderer.setAnimationLoop((time) => {
+  //controls.update(0.0001 * time); // for firstPersonControls
   grass.update(time)
   renderer.render(scene, camera)
 })
---
+++ skybox.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three'
+
+// Made with the help of: https://codinhood.com/post/create-skybox-with-threejs/
+// Class for a Skybox
+export class Skybox extends THREE.Mesh{
+    // Constant part of the path to Skybox textures
+    textureBasePath = "textures/daylightBox";
+
+    constructor() {
+        super();
+        let skyboxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
+        let skyboxMaterials = this.createMaterialArray(this.textureBasePath);
+        // Create skybox mesh
+        this.add(new THREE.Mesh(skyboxGeometry, skyboxMaterials));
+    }
+
+    /**
+     * Creates the file path strings for each texture of a Skybox
+     * @param {*} fileBasePath - The constant part of the file path
+     * @returns - An array of file path strings
+     */
+    createFilePaths(fileBasePath) {
+        let fileType = ".bmp";
+        let sides = ["Front", "Back", "Top", "Bottom", "Left", "Right"];
+        let filePaths = [];
+        // For each texture
+        for (let side in sides) {
+            // Create the corresponding filepath
+            filePaths.push(fileBasePath + "_" + sides[side] + fileType);
+        }
+        return filePaths;
+    }
+
+    /**
+     * Creates an array of mesh materials for each side of a Skybox
+     * @param {*} fileBasePath - The constant part of the file path to the textures
+     * @returns - An array of MeshBasicMaterials storing the Skybox's textures
+     */
+    createMaterialArray(fileBasePath) {
+        let filePaths = this.createFilePaths(fileBasePath);
+        let materialArray = [];
+        // For each texture
+        for (let file in filePaths) {
+            // Load the texture
+            let texture = new THREE.TextureLoader().load(filePaths[file]);
+            // Map the texture to the backside of a mesh material
+            let mesh = new THREE.MeshBasicMaterial({map: texture, side: THREE.BackSide}) 
+            materialArray.push(mesh);
+        }
+        return materialArray;
+    }
+}
