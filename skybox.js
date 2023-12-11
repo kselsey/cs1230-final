1e84540eea77e1548425169eb96b1efa4499dfcb
lmursalova <lmursalova803@gmail.com>
2023-12-11 16:02:53 -0500

    fix: changed to daylight skybox

--- skybox.js
+++ skybox.js
@@ -4,7 +4,7 @@ import * as THREE from 'three'
 // Class for a Skybox
 export class Skybox extends THREE.Mesh{
     // Constant part of the path to Skybox textures
-    textureBasePath = "textures/skyboxOptions/spacePaint";
+    textureBasePath = "textures/skyboxOptions/skyWithGroundPaint";
 
     constructor() {
         super();
